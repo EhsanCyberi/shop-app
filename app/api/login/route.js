a7f25c56e287d3fbbff5e25c2e8e188a00880445
Ehsan Cyberi <ehsancyberi@gmail.com>
2026-02-26 02:24:32 +0330

    Update next, v1.2 bismilah rehmanrahim 111

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -40,7 +40,7 @@ export async function POST(req) {
     response.cookies.set("token", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "lax",
+        sameSite: "none",
         path: "/",
     });
     return response;
